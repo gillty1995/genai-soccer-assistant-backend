b3d00f766ee6c77187884eb269ed1d9017a61044
gillty <gooee@mac.com>
2024-12-22 14:55:52 -0600

    refactor limiter

--- src/server.ts
+++ src/server.ts
@@ -34,15 +34,11 @@ const corsOptions = {
 
 // limiter
 const limiter = rateLimit({
-  windowMs: 15 * 60 * 1000, // 15 minutes
-  max: 100, // limit each IP to 100 requests per windowMs
+  windowMs: 15 * 60 * 1000, 
+  max: 100, 
   message: "Too many requests, please try again later.",
   standardHeaders: true, 
   legacyHeaders: false,  
-  keyGenerator: (req) => {
-    const forwardedFor = req.headers['x-forwarded-for'] as string;
-    return forwardedFor || req.ip || "unknown";
-  },
 });
 
 // Middlewares
